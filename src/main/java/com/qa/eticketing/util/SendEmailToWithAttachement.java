a9792213a575b2773f6e6864d2008a6e7d08663e
unknown <mayami0110@gmail.com>
2020-05-15 19:32:34 +0530

    mail password

--- src/main/java/com/qa/eticketing/util/SendEmailToWithAttachement.java
+++ src/main/java/com/qa/eticketing/util/SendEmailToWithAttachement.java
@@ -216,11 +216,11 @@ public class SendEmailToWithAttachement {
 	class SMTPAuthenticator extends Authenticator {
 		private static final String SMTP_AUTH_USER = "mayank_mishra";
 
-		private static final String SMTP_AUTH_PASSWORD = "Cms@12345";
+		private static final String SMTP_AUTH_PASSWORD = "Cms@123456";
 
 		public PasswordAuthentication getPasswordAuthentication() {
 			String username = "mayank_mishra";
-			String password = "Cms@12345";
+			String password = "Cms@123456"; // updated
 			return new PasswordAuthentication(username, password);
 		}
 	}
